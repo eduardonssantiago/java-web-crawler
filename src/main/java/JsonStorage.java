ba9eec9788eccfadf202eaccbdc2602e8cb7878b
eduardonssantiago <eduardo.ns.santiago@gmail.com>
2026-03-26 09:43:10 -0300

    fix: add Instant TypeAdapter to Gson for JSON

--- src/main/java/JsonStorage.java
+++ src/main/java/JsonStorage.java
@@ -1,13 +1,21 @@
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.JsonPrimitive;
+import com.google.gson.JsonSerializer;
+
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.io.IOException;
+import java.time.Instant;
 import java.time.LocalDate;
 
 public class JsonStorage {
 
-    private final Gson gson = new GsonBuilder().setPrettyPrinting().create();
+    private final Gson gson = new GsonBuilder()
+            .setPrettyPrinting()
+            .registerTypeAdapter(Instant.class,
+                    (JsonSerializer<Instant>) (src, typeOfSrc, context) -> new JsonPrimitive(src.toString()))
+            .create();
 
     public void save(PageContent pageContent) {
         String fileName = LocalDate.now() + ".json";
