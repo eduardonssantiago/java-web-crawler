17374dfb81a0e83272d72c7d91002d33d723e05c
eduardonssantiago <eduardo.ns.santiago@gmail.com>
2026-03-26 09:58:57 -0300

    feat: complete web crawler pipeline with JSON output

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,7 +1,22 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class App {
 
     public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
         CrawlPipeline crawlPipeline = new CrawlPipeline();
-        crawlPipeline.execute("http://info.cern.ch");
+        ArrayList<String > websites = new ArrayList<>();
+
+        for(int i = 0; i < 3; i++) {
+            System.out.println("Website: ");
+            String website = scanner.nextLine();
+            websites.add(website);
+        }
+
+        for(int i = 0; i < 3; i++) {
+            crawlPipeline.execute(websites.get(i));
+        }
     }
 }
